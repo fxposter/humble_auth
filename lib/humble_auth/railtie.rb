d7b72eef7c8d4b9b1cf1890705a92264196e01b9
Pavel Forkert <fxposter@gmail.com>
2011-10-07 22:35:59 +0300

    fixes

--- lib/humble_auth/railtie.rb
+++ lib/humble_auth/railtie.rb
@@ -4,9 +4,13 @@ require 'humble_auth/helper'
 module HumbleAuth
   class Railtie < Rails::Railtie
     initializer 'humble_auth.initialize' do
-      config_file = Rails.root.join('config', 'auth.yml')
-      puts 'Write your authentication data into config/auth.yml file!' unless File.exists?(config_file)
-      config.auth = HumbleAuth::Auth.make_config(YAML.load_file(config_file)[Rails.env]['auth'])
+      config_file = Rails.root.join('config', 'auth.yml').to_s
+      
+      if File.exists?(config_file)
+        config.auth = HumbleAuth::Auth.make_config(YAML.load_file(config_file)[Rails.env]['auth'])
+      else
+        puts 'Write your authentication data into config/auth.yml file!'
+      end
       
       ActiveSupport.on_load :action_controller do
         include HumbleAuth::Helper
